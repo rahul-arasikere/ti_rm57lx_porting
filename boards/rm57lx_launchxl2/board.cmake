429650ed2760ba9fa5550defb804ba55f5b4d170
Rahul Arasikere <arasikere.rahul@gmail.com>
2025-09-07 22:30:11 -0400

    added openocd debug command

--- boards/rm57lx_launchxl2/board.cmake
+++ boards/rm57lx_launchxl2/board.cmake
@@ -1,4 +1,7 @@
 # Copyrights (c) 2024 Rahul Arasikere <arasikere.rahul@gmail.com>
 # SPDX-License-Identifier: Apache-2.0
 
+board_runner_args(openocd "transport select jtag")
+board_runner_args(openocd "source [find xds110.cfg")
+
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
